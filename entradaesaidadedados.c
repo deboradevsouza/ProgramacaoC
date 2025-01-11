6584467af3e6895e391916de16ca9db8a702359c
deboradevsouza <59833039+deboradevsouza@users.noreply.github.com>
2025-01-11 22:17:42 +0000

    segundo commit

---
+++ entradaesaidadedados.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+
+    int idade = 30;
+    float altura = 1.60;
+    char opcao = 'A';
+    char nome[20] = "Debora" ;
+
+    printf("Idade: %d\n ", idade);
+    printf("Altura: %f\n ", altura);
+    printf("Opção: %c\n ", opcao);
+    printf("Nome: %s\n ", nome);
+
+    /*
+    printf("%formato1 %formato 2 %formato3", variavel1 variavel2 variavel3)
+    
+    %d: Imprime um inteiro no formato decimal.
+    %i: Equivalente a %d.
+    %f: Imprime um número de ponto flutuante no formato padrão.
+    %e: Imprime um número de ponto flutuante na notação científica.
+    %c: Imprime um único caractere.
+    %s: Imprime uma cadeia (string) de caracteres.
+    */
+}
+
